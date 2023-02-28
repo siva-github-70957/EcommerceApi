d6bb6736a798e996cdd3849795cd92f64df4e9d3
siva-github-70957 <92137712+siva-github-70957@users.noreply.github.com>
2023-02-28 22:18:22 +0530

    update cors acc to netlify

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'https://bliss-ecommerce.netlify.app/'
 }))
 
 cloudinary.config({
